4cf0e6919302d9cea2d9af10cbc151d57e58d390
Adam Labadorf <alabadorf@gmail.com>
2010-11-01 14:13:02 -0400

    added textwrapping to generate_reademe.py, regenerated README

--- examples/generate_readme/generate_readme.py
+++ examples/generate_readme/generate_readme.py
@@ -1,25 +1,32 @@
+import textwrap
+
 from reStUtil import *
 
 doc = ReStDocument('README') # accepts file pointer or filename string
 
 doc.add(ReStSection('Introduction')) # add a top level section
 
-doc.add("""reStructuredText_ is a simple markup syntax for producing human-readable and
-easily machine-parsable documents. This python package provides a simple
-programmatic API to produce reStructuredText-formatted documents.  It is useful
+intro = """reStructuredText_ is a simple markup syntax for producing human-readable
+and easily machine-parsable documents. This python package provides
+a simple API to produce reStructuredText-formatted documents.  It is useful
 to, for example, generate human readable reports from within scripts, rather
 than generating intermediate data files that are then later transformed.
 reStructuredText directives are represented as python objects that can be
 arbitrarily nested for structural and/or organizational purposes.  It is far
 from complete, but has many useful directives, including sections, tables,
-figures, and hyperlinks.""")
+figures, and hyperlinks."""
+
+doc.add(textwrap.fill(intro))
 
 doc.add(ReStHyperlink('reStructuredText',url='http://docutils.sourceforge.net/rst.html'))
 
+
 doc.add(ReStSection('Examples'))
 
-doc.add("""There is a growing number of real examples in the *examples/*
+examples = """There is a growing number of real examples in the *examples/*
 directory under this repository.  Look there for uses of the various directive
-classes""")
+classes"""
+
+doc.add(textwrap.fill(examples))
 
 doc.write()
