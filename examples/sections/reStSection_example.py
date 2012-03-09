3270ead3b34695c54bd8d3f3c4094025109b1ae2
Adam Labadorf <alabadorf@gmail.com>
2012-03-09 13:54:29 -0500

    updated example to use new container methods put on section

--- examples/sections/reStSection_example.py
+++ examples/sections/reStSection_example.py
@@ -4,28 +4,32 @@ from reStUtil import ReStDocument, ReStSection, ReStTable
 doc = ReStDocument(sys.stdout)
 
 # section 1
-doc.add(ReStSection("Section 1",level=1))
-doc.add("The text in section 1")
+sec1 = ReStSection("Section 1",level=1)
+sec1.add("The text in section 1")
 
-doc.add(ReStSection("Subsection 1.1",level=2))
-doc.add("The text in subsection 1.1")
+sec1_1 = ReStSection("Subsection 1.1",level=2)
+sec1_1.add("The text in subsection 1.1")
+sec1 += sec1_1
 
-doc.add(ReStSection("Section 2",level=1))
-doc.add("The text in section 2")
+sec2 = ReStSection("Section 2",level=1)
+sec2.add("The text in section 2")
 
-doc.add(ReStSection("Subsection 2.1",level=2))
-doc.add("The text in subsection with user specified heading char")
+sec2_1 = ReStSection("Subsection 2.1",level=2)
+sec2_1.add("The text in subsection 2.1")
 
-doc.add(ReStSection("Subsection 2.2",level=2))
-doc.add("Text in another subsection with user char")
+sec2_2 = ReStSection("Subsection 2.2",level=2)
+sec2_2.add("Text in subsection 2.2")
+sec2.add(sec2_1,sec2_2)
 
-doc.add(ReStSection("Section 3",level=1))
-doc.add("This section has many nested subsections")
+sec3 = ReStSection("Section 3",level=1)
+sec3.add("This section has many nested subsections")
 
-doc.add(ReStSection("Subsection 3.1",level=2))
-doc.add(ReStSection("Subsubsection 3.1.1",level=3))
-doc.add(ReStSection("Subsubsubsection 3.1.1.1",level=4))
-doc.add(ReStSection("Subsubsubsubsection 3.1.1.1.1",level=5))
-doc.add(ReStSection("Subsubsubsubsubsection 3.1.1.1.1.1",level=6))
+sec3.add(ReStSection("Subsection 3.1",level=2))
+sec3.add(ReStSection("Subsubsection 3.1.1",level=3))
+sec3.add(ReStSection("Subsubsubsection 3.1.1.1",level=4))
+sec3.add(ReStSection("Subsubsubsubsection 3.1.1.1.1",level=5))
+sec3.add(ReStSection("Subsubsubsubsubsection 3.1.1.1.1.1",level=6))
+
+doc.add(sec1,sec2,sec3)
 
 doc.write()
