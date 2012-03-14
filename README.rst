c6f73f3ea276715e196968701e832d3dc8fcdcc1
Adam Labadorf <alabadorf@gmail.com>
2012-03-14 16:31:12 -0400

    added API doc link to readme

--- README.rst
+++ README.rst
@@ -6,21 +6,21 @@ reStUtil python package
 Introduction
 ============
 
-
-reStructuredText_ is a simple markup syntax for producing human- readable and
+reStructuredText_ is a simple markup syntax for producing human-readable and
 easily machine-parsable documents. This python package provides a simple API to
-produce reStructuredText-formatted documents. It is useful to, for example,
+produce reStructuredText-formatted documents.  It is useful to, for example,
 generate human readable reports from within scripts, rather than generating
 intermediate data files that are then later transformed. reStructuredText
 directives are represented as python objects that can be arbitrarily nested for
 structural and/or organizational purposes.  It is far from complete, but has
 many useful directives, including sections, tables, figures, and hyperlinks.
 
-`API documentation`_.
+`API Documentation`_
 
 .. _reStructuredText: http://docutils.sourceforge.net/rst.html
 
 .. _API Documentation: http://adamlabadorf.github.com/reStUtil/
+
 
 
 Examples
--- examples/generate_readme/generate_readme.py
+++ examples/generate_readme/generate_readme.py
@@ -2,9 +2,10 @@ import textwrap
 
 from reStUtil import *
 
-doc = ReStDocument('README',title='reStUtil python package') # accepts file pointer or filename string
+doc = ReStDocument('README.rst',title='reStUtil python package') # accepts file pointer or filename string
 
-doc.add(ReStSection('Introduction')) # add a top level section
+intro_sec = ReStSection('Introduction')
+doc.add(intro_sec) # add a top level section
 
 intro = """reStructuredText_ is a simple markup syntax for producing human-readable
 and easily machine-parsable documents. This python package provides
@@ -15,14 +16,14 @@ reStructuredText directives are represented as python objects that can be
 arbitrarily nested for structural and/or organizational purposes.  It is far
 from complete, but has many useful directives, including sections, tables,
 figures, and hyperlinks.
-
-API documentation `here<api>`_.
 """
 
-doc.add(textwrap.fill(intro))
+intro_sec.add(intro)
 
-doc.add(ReStHyperlink('reStructuredText',url='http://docutils.sourceforge.net/rst.html'))
-doc.add(ReStHyperlink('api',url='http://adamlabadorf.github.com/reStUtil/'))
+intro_sec.add("`API Documentation`_")
+
+intro_sec.add(ReStHyperlink('reStructuredText',url='http://docutils.sourceforge.net/rst.html'))
+intro_sec.add(ReStHyperlink('API Documentation',url='http://adamlabadorf.github.com/reStUtil/'))
 
 doc.add(ReStSection('Examples'))
 
@@ -30,6 +31,6 @@ examples = """There is a growing number of real examples in the *examples/*
 directory under this repository.  Look there for uses of the various directive
 classes"""
 
-doc.add(textwrap.fill(examples))
+doc.add(examples)
 
 doc.write()
